a4eacea30480cf2d9c49065d17128e55a499562d
Ismael P. <iperez08@gmail.com>
2024-09-17 12:13:50 -0400

    create student model, serializer, and createstudentview

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, CreateStudentView
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
+  path('students/register', CreateStudentView.as_view(), name='student-register')
 ]
