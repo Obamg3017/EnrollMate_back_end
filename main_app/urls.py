e24178cb1ed97c01fa590a2c95234263e4cff774
Ismael P. <iperez08@gmail.com>
2024-09-17 12:31:45 -0400

    home rename to home_page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Home, CreateStudentView
 
 urlpatterns = [
-  path('', Home.as_view(), name='home'),
+  path('', Home.as_view(), name='home_page'),
   path('students/register', CreateStudentView.as_view(), name='student-register')
 ]
