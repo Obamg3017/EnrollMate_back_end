aec460697dcecbd8b74a9f00a426b1a30e641394
FemiBam <Femibam1@gmail.com>
2024-09-17 13:59:44 -0400

    completed Login View and updated created user view

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from .models import Student
-from .serializers import StudentSerializer
+from .serializers import StudentSerializer, UserSerializer
 
 class CreateStudentView(generics.CreateAPIView):
   queryset = Student.objects.all()
@@ -18,6 +18,7 @@ class CreateStudentView(generics.CreateAPIView):
     return Response({
       'refresh': str(refresh),
       'access': str(refresh.access_token),
+      'user': UserSerializer(user).data,
       'student': response.data,
     })
 
@@ -27,3 +28,26 @@ class Home(APIView):
   def get(self, request):
     content = {'message': 'Welcome to the course-collector api home route!'}
     return Response(content)
+  
+class LoginView(APIView):
+  permission_classes = [permissions.AllowAny]
+
+  def post(self, request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    if user:
+      refresh = RefreshToken.for_user(user)
+      try:
+        student = Student.objects.get(user=user)
+        student_data = StudentSerializer(student).data
+      except Student.DoesNotExist:
+        student_data = None  
+      
+      return Response({
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+        'user': UserSerializer(user).data,
+        'student': student_data  
+      })
+    return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
